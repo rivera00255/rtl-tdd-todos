3d06d6185a49a7d1f77934f2482914de0e982195
rivera00255 <rivera00255@gmail.com>
2022-02-22 07:55:04 +0000

    TodoApp new TodoItem test ok

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
@@ -17,9 +17,22 @@ const TodoApp = () => {
         }
     ]);
 
+    const nextId = useRef(3); //새로 추가할 항목에서 사용할 id
+    const onInsert = useCallback(
+        text => {
+            setTodos(todos.concat(
+                {
+                    id: nextId.current,
+                    text,
+                    done : false
+                }
+            ))
+            nextId.current += 1;
+    }, [todos]);
+
     return(
         <div>
-            <TodoForm data-testid='helloworld' />
+            <TodoForm data-testid='helloworld' onInsert={onInsert} />
             <TodoList todos={todos} />
         </div>
     );
--- src/TodoApp.test.js
+++ src/TodoApp.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import TodoApp from './TodoApp';
 
 describe('<TodoApp />', () => {
@@ -12,5 +12,11 @@ describe('<TodoApp />', () => {
         const {getByText} = render(<TodoApp />);
         getByText('TDD 배우기');
         getByText('react-testing-library 사용하기');
+    });
+    it('creates new todo', () => {
+        const {getByPlaceholderText, getByText} = render(<TodoApp />);
+        fireEvent.change(getByPlaceholderText('할 일을 입력하세요.'), {target: {value: '새 항목 추가 하기'}});
+        fireEvent.click(getByText('등록'));
+        getByText('새 항목 추가 하기');
     });
 })
