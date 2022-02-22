1148f7ce384628c045991d54f5cee47c63f73876
rivera00255 <rivera00255@gmail.com>
2022-02-22 02:27:35 +0000

    TodoForm input test ok

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react'; // react-testing-library;
+import { fireEvent, render } from '@testing-library/react'; // react-testing-library;
 import TodoForm from './TodoForm';
 
 describe('<TodoForm />', () => {
@@ -7,5 +7,12 @@ describe('<TodoForm />', () => {
         const {getByText, getByPlaceholderText} = render(<TodoForm />);
         getByPlaceholderText('할 일을 입력하세요.'); //input tag 확인
         getByText('등록'); // button이 있는지 확인
+    })
+    it('change input', () => {
+        const {getByPlaceholderText} = render(<TodoForm />);
+        const input = getByPlaceholderText('할 일을 입력하세요.');
+
+        fireEvent.change(input, {target: {value: 'TDD 배우기'}});
+        expect(input).toHaveAttribute('value', 'TDD 배우기');
     })
 })
