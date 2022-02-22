247f68aa39d6cd3f83f5490bdc8d02ef034b97f8
rivera00255 <rivera00255@gmail.com>
2022-02-22 07:31:03 +0000

    TodoApp TodoList render test ok

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 const TodoApp = () => {
+
+    const [todos, setTodos] = useState([
+        {
+            id : 1,
+            text : 'TDD 배우기',
+            done : true
+        },
+        {
+            id : 2,
+            text : 'react-testing-library 사용하기',
+            done : true
+        }
+    ]);
+
     return(
         <div>
             <TodoForm data-testid='helloworld' />
-            <TodoList todos={[]} />
+            <TodoList todos={todos} />
         </div>
     );
 }
--- src/TodoApp.test.js
+++ src/TodoApp.test.js
@@ -8,4 +8,9 @@ describe('<TodoApp />', () => {
         getByText('등록');
         getByTestId('TodoList'); 
     });
+    it('renders two defaults todos', () => {
+        const {getByText} = render(<TodoApp />);
+        getByText('TDD 배우기');
+        getByText('react-testing-library 사용하기');
+    });
 })
