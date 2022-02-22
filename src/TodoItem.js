781f5757a4ccb5e676c090b1ffcc69e182cbe490
rivera00255 <rivera00255@gmail.com>
2022-02-22 06:06:04 +0000

    TodoItem click event test ok

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const TodoItem = ({todo}) => {
+const TodoItem = ({todo, onToggle, onRemove}) => {
 
     const {id, text, done} = todo;
 
     return(
         <li>
-            <span>{text}</span>
-            <button>삭제</button>
+            <span 
+                style={{textDecoration : done ? 'line-through' : 'none'}}
+                onClick={() => onToggle(id)}
+            >{text}</span>
+            <button onClick={() => onRemove(id)}>삭제</button>
         </li>
     );
 }
