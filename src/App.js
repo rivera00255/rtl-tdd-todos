5d506c27b600e11bf22ddc3d7544f88009a6026e
rivera00255 <rivera00255@gmail.com>
2022-02-24 01:43:01 +0000

    jenkins with github branch main

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import TodoApp from './TodoApp';
 function App() {
   return (
     <div className="App">
-      <h2>jenkins with github</h2>
-      <h3>jenkins</h3>
+      <h2>jenkins with github branch main</h2>
       <TodoApp />
     </div>
   );
